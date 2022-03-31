c1c163f06be8bffc9c908be368f368e18e1eb396
VictorMarinFelip <vfelip@aprawa.com>
2022-03-31 20:05:11 +0200

    chore: pep8

--- trading_arbitrator/amm.py
+++ trading_arbitrator/amm.py
@@ -1,7 +1,8 @@
 import math
-from errors import LPDepletedError
+from trading_arbitrator.errors import LPDepletedError
 
-def ConstantProductAMM(i, j, am, state, **kwargs):
+
+def constant_product_amm(i, j, am, state):
     """
     Implements prod(x_i) = C, where x_i are the amounts of i coins in the pool,
     and C a constant. Constant product automated market maker.
@@ -10,22 +11,21 @@ def ConstantProductAMM(i, j, am, state, **kwargs):
     :param j: index of target coin
     :param am: amount of starting coin to trade for target coin
     :param state: list of pool sizes
-    :param kwargs: Extra stuff that the function may need
     :return: amount of target asset to be sent to trader
     """
     initial_j = state[j]
     # We get the constant C
-    C = math.prod(state)
+    c = math.prod(state)
     # We add "am" quantity of coin i to the pool:
     state[i] += am
     prod_no_j = math.prod(state[:j]+state[j+1:])
-    final_j = C / prod_no_j
+    final_j = c / prod_no_j
     # We write the new amount of j
     state[j] = final_j
     return initial_j - final_j
 
 
-def ConstantSumAMM(i, j, am, state, **kwargs):
+def constant_sum_amm(i, j, am, state):
     """
     Implements sum(x_i) = C, where x_i are the amounts of i coins in the pool,
     and C a constant. Constant sum automated market maker. This implements a
@@ -35,16 +35,15 @@ def ConstantSumAMM(i, j, am, state, **kwargs):
     :param j: index of target coin
     :param am: amount of starting coin to trade for target coin
     :param state: list of pool sizes
-    :param kwargs: Extra stuff that the function may need
     :return: amount of target asset to be sent to trader
     """
     initial_j = state[j]
     # We get the constant C
-    C = sum(state)
+    c = sum(state)
     # We add "am" quantity of coin i to the pool:
     state[i] += am
     sum_no_j = sum(state[:j]+state[j+1:])
-    final_j = C - sum_no_j
+    final_j = c - sum_no_j
     if final_j < 0:
         raise LPDepletedError
     # We write the new amount of j
